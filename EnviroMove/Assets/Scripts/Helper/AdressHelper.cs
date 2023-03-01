921c98ba1a121964ff400f80323dd30b2b1cc38b
Théo M <t.marcuzzi@rubika-edu.com>
2023-03-01 01:04:28 +0100

    hish

--- EnviroMove/Assets/Scripts/Helper/AdressHelper.cs
+++ EnviroMove/Assets/Scripts/Helper/AdressHelper.cs
@@ -7,15 +7,12 @@ public static class AdresseHelper
 {
     public static void LoadAssetWithCallback<T>(string adress, Action<T> callbackAction)
     {
-        Debug.Log("Alala");
         var callback = Addressables.LoadAssetAsync<T>(adress);
-        Debug.Log($"Callback : {callback.DebugName}");
         callback.Completed += (_) => OnLoadedAssetAsync(adress, _, callbackAction);
     }
 
     static void OnLoadedAssetAsync<T>(string key, AsyncOperationHandle<T> handle, Action<T> callbackAction)
     {
-        Debug.Log("Assync Load");
         if (handle.Status == AsyncOperationStatus.Succeeded)
         {
             callbackAction.Invoke(handle.Result);
