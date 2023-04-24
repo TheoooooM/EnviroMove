422b429abe51883e1592b7f66c88458bf702fd77
Theo_M <t.marcuzzi@rubika-edu.com>
2023-04-24 12:25:29 +0200

    PlayerMove

--- EnviroMove/Assets/Scripts/Player/Player.cs
+++ EnviroMove/Assets/Scripts/Player/Player.cs
@@ -9,38 +9,47 @@ public class Player : MonoBehaviour, IBoardable
     private IBoard _board;
     
     private Vector3Int _boardPos;
-    private Enums.Side _lookDir = Enums.Side.top;
     private Vector3 _moveDir;
 
-    private bool moving;
+    private Enums.Side _lastDir = Enums.Side.top;
+    private bool _moving;
+    private basicDelegate _onMoveFinish;
     
-    [SerializeField] private float speed;
+    [SerializeField] private float moveSpeed;
     
     [SerializeField] protected List<Enums.BlockTag> tags;
     public List<Enums.BlockTag> GetTags() => tags;
 
 
-    void UpdateAction()
+    
+
+    void Move()
     {
-        if (!moving)
+        var dir = _board.GetPlayerDirection(_boardPos);
+        if (dir != Enums.Side.none) _lastDir = dir;
+        if (_board.TryMove(_boardPos, _lastDir, out Vector3 movePosition))
         {
-            if (_board.TryMove(_boardPos, _lookDir, out Vector3 movePosition))
-            {
-                moving = true;
-                StartCoroutine(MoveToPoint(movePosition));
-            }
+            _moving = true;
+            StartCoroutine(MoveToPoint(movePosition));
         }
     }
    
 
-    IEnumerator MoveToPoint(Vector3 position)
+    IEnumerator MoveToPoint(Vector3 newPos)
     {
-        while (Vector3.Distance(transform.position, position)< speed)
-        {
-            transform.position += _moveDir*speed;
-            yield return new WaitForEndOfFrame();
-        }
-        moving = false;
+        var magnitude = Vector3.Distance(transform.position, newPos);
+            var startMagnitude = magnitude;
+            var step = moveSpeed * Time.deltaTime;
+            while (magnitude> step)
+            {
+                //Debug.Log($"Moving by {(newPos - transform.position).normalized * step}");
+                transform.position += (newPos - transform.position).normalized * step;
+                magnitude -= step;
+                yield return new WaitForEndOfFrame();
+            }
+            transform.position = newPos;
+            _moving = false;
+            _onMoveFinish?.Invoke();
     }
     public void SetOnBoard(Vector3Int boardPos, IBoard board)
     {
@@ -60,6 +69,7 @@ public class Player : MonoBehaviour, IBoardable
 
     public void StartBoard()
     {
-        //UpdateAction();
+        Move();
+        _onMoveFinish += Move;
     }
 }
