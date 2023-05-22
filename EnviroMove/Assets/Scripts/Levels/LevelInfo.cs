3eb4c459a14821acd56b6bf6dcb6b7bafa9b8c90
Theo_M <t.marcuzzi@rubika-edu.com>
2023-05-22 16:57:10 +0200

    Ouai je taff sur la DB (mais j'ai fiat les fx promis)

--- EnviroMove/Assets/Scripts/Levels/LevelInfo.cs
+++ EnviroMove/Assets/Scripts/Levels/LevelInfo.cs
@@ -16,7 +16,7 @@
 
             public int difficulty;
 
-            public int Season;
+            public int season;
 
             public bool wasTrending;
             public bool wasDaysMap;
@@ -29,6 +29,23 @@
                 this.creator = creator;
                 this.levelFilePath = levelFilePath;
                 this.id = id;
+            }
+
+            public LevelInfo(string levelName, string creator, string id, int like, int weekLike, int carrotAmount,
+                int goldValue, int difficulty, int season, bool wasTrending, bool wasDaysMap, int timesPlay)
+            {
+                this.levelName = levelName;
+                this.creator = creator;
+                this.id = id;
+                this.like = like;
+                this.weekLike = weekLike;
+                this.carrotAmount = carrotAmount;
+                this.goldValue = goldValue;
+                this.difficulty = difficulty;
+                this.season = season;
+                this.wasTrending = wasTrending;
+                this.wasDaysMap = wasDaysMap;
+                this.timesPlay = timesPlay;
             }
         }
     }
